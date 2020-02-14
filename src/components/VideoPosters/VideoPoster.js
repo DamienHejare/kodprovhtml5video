34fb85f8c8f44d5bc4d86a321edc58ec188d7422
Damien Jones <damienjones@Damiens-MacBook-Pro.local>
2020-02-14 18:48:06 +0100

    added tooltip and connected image to context

--- src/components/VideoPosters/VideoPoster.js
+++ src/components/VideoPosters/VideoPoster.js
@@ -1,18 +1,44 @@
 import React, { useContext } from "react";
 import MainContext, { ACTION_TYPE } from "../../store/MainContext";
-import './VideoPoster.css'
+import "./VideoPoster.css";
+import Tippy from "@tippy.js/react";
+import { followCursor } from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/dist/backdrop.css";
+import "tippy.js/animations/shift-away.css";
+import "tippy.js/themes/light.css";
 
-const VideoPoster = () => {
-  const { state, dispatch } = useContext(MainContext);
+const VideoPoster = ({ video }) => {
+  const { dispatch } = useContext(MainContext);
 
   return (
-    <div className="VideoPoster">
-      <img src={"https://iptvimagecache-playplus-prod-vip.han.telia.se/crop/200x290/http%3A%2F%2Fiptvlogin.telia.se%2F%2FSF%20Anytime%2F4749441.jpg"} className="Poster" alt="logo" />
-    </div>
+    <Tippy
+      // options
+      theme="light"
+      appendTo={document.body}
+      content={video.description}
+      placement="top-start"
+      trigger="mouseenter"
+      followCursor={true}
+      flipOnUpdate={true}
+      arrow={false}
+      plugins={[followCursor]}
+      boundary="window"
+    >
+      <div
+        className="VideoPoster"
+        onClick={() =>
+          dispatch({
+            type: ACTION_TYPE.CHANGE_SELECTED_VIDEO,
+            payload: { selectedVideo: video }
+          })
+        }
+      >
+        <img src={video.image} className="VideoPosterImg" alt={video.name} />
+        <div className="VideoPosterName">{video.name}</div>
+      </div>
+    </Tippy>
   );
 };
 
 export default VideoPoster;
-
-
-   
