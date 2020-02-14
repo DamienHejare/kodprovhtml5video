139a824a1ff9ef7be6cdc7b62954d6c75c9c0d61
Damien Jones <damienjones@Damiens-MacBook-Pro.local>
2020-02-14 18:47:16 +0100

    added window resizing and connected source to context

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,19 +1,40 @@
-import React, { useContext } from "react";
-import MainContext, { ACTION_TYPE } from "../../store/MainContext";
-import './VideoPlayer.css';
+import React, { useContext, useState, useEffect, useRef } from "react";
+import MainContext from "../../store/MainContext";
+import "./VideoPlayer.css";
 
 const VideoPlayer = () => {
-  const { state, dispatch } = useContext(MainContext);
+  const { state } = useContext(MainContext);
+  const [ videoPlayerWidth, setVideoPlayerWidth] = useState(836)
+  
+  const videoPlayerRef = useRef(null)
 
+  useEffect(() => {
+    function getSize() {
+      console.log(window.innerWidth)
+      return window.innerWidth
+      
+    }
+  
+    function handleResize() {
+      const maxWidth = Math.min(getSize(), 836)
+      setVideoPlayerWidth(maxWidth);
+      videoPlayerRef.current.style.maxWidth = maxWidth + 'px'
+    }
+
+
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+
+  }, [])
   return (
-    <div className="VideoPlayer">
-      <video width="320" height="240" controls>
-        <source
-          src="http://mirrors.standaloneinstaller.com/video-sample/TRA3106.mp4"
-          type="video/mp4"
-        ></source>
-        Your browser does not support the video tag.
-      </video>
+    <div className="VideoPlayer" ref={videoPlayerRef}>
+      {state.selectedVideoSrc !== null && (
+        <video width={videoPlayerWidth} controls autoPlay key={state.selectedVideoSrc}>
+          <source src={state.selectedVideoSrc} type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+      )}
     </div>
   );
 };
